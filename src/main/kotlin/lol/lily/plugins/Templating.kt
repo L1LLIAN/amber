0f57b2eb8f47cf03bfe3e579b68b648d2e47510a
Lillian Armes <me@lily.lol>
2022-06-22 10:25:38 -0400

    cache fix maybe pls

--- src/main/kotlin/lol/lily/plugins/Templating.kt
+++ src/main/kotlin/lol/lily/plugins/Templating.kt
@@ -24,6 +24,31 @@ fun Application.configureTemplating() {
                 title = "Amber"
 
                 head {
+                    meta {
+                        httpEquiv = "cache-control"
+                        content = "max-age=0"
+                    }
+
+                    meta {
+                        httpEquiv = "cache-control"
+                        content = "no-cache"
+                    }
+
+                    meta {
+                        httpEquiv = "expires"
+                        content = "0"
+                    }
+
+                    meta {
+                        httpEquiv = "expires"
+                        content = "Tue, 01 Jan 1980 1:00:00 GMT"
+                    }
+
+                    meta {
+                        httpEquiv = "pragma"
+                        content = "no-cache"
+                    }
+
                     meta("viewport") {
                         content = "width=device-width, minimum-scale=0.1"
                     }
